20787dbfa2ec333720e14d50c0512aad3db273be
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-04-14 23:15:18 +0530

    completed one lecture

---
+++ TS1.ts
@@ -0,0 +1,8 @@
+// name jo hai wo value string me aegi
+// and jo function return kr k value jaa rhi hai wo string me aegi 
+function greet(name: string): string{
+    return `Hello, ${name}`;
+}
+
+console.log(greet("hitesh"));
+console.log(greet(42));
