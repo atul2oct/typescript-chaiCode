159641a13291891e59289cd4741da42d2c916cbd
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-04-18 16:22:03 +0530

    lecture 3 completed
    how ts works with node npm npx

---
+++ class 3/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Helo ${person} , Welcome to chai aur typescript`;
+}
+const username = "Atul";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ class 3/src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string{
+    return `Helo ${person} , Welcome to chai aur typescript`
+}
+
+const username: string = "Atul";
+console.log(greet(username));
