a7a9bf053339fbaf46bc40c55a25b1bfa645d91e
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-04-20 01:28:16 +0530

    any

---
+++ class 3/src/unionAndany.ts
@@ -0,0 +1,20 @@
+let subs: number | string = '1M'; // union  | no ya string koi bhi
+ let apiRequestStatus: 'pending' | 'success' | 'error' = 'pending'; //iski value yeh 3 option k alawa hoi hi nhi sakti hai
+//  apiRequestStatus = 'done' //error Type '"done"' is not assignable to type '"pending" | "success" | "error"'
+apiRequestStatus = 'success';
+
+// any -> avoid how much you can
+const orders = ['12','20','28','30'];
+// both are same
+// let currentOrder: any; //any 
+// let currentOrder; //any 
+let currentOrder: string | undefined;//undeinfe is edge case
+for(let order of orders){
+    if(order === '28'){
+        currentOrder = order;
+        break;
+    }
+}
+
+// currentOrder = 42; //yeh any hai isliye koi bhi value aa gai but we want only string so currenOrder we type define to string
+console.log(currentOrder)
