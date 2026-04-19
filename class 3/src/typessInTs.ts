4643cec6c0314d2e664188f3bb96deb4d1aedd64
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-04-20 01:03:11 +0530

    Update typessInTs.ts

--- class 3/src/typessInTs.ts
+++ class 3/src/typessInTs.ts
@@ -1,9 +1,9 @@
-// inferencing
+// inferencing means apne aap type lelega
 let  drink = "chai"
 // drink =0;
 let cups = Math.random() > 0.5 ? 10 : "0.5";
 
-// typeannotation collon laga kr annotate kr diya
+// typeannotation(batana padega types) collon laga kr annotate kr diya
 let chaiFlavour : string = "masala chai"
 chaiFlavour = "Ginger tea"
 // kya nhi chalega
