9d31d49016c7af6efaf7693c910ac85998a704bd
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2026-04-19 16:41:44 +0530

    typeannotation

--- class 3/src/typessInTs.ts
+++ class 3/src/typessInTs.ts
@@ -3,4 +3,6 @@ let  drink = "chai"
 // drink =0;
 let cups = Math.random() > 0.5 ? 10 : "0.5";
 
-// typeannotation
+// typeannotation collon laga kr annotate kr diya
+let chaiFlavour : string = "masala chai"
+chaiFlavour = "Ginger tea"
